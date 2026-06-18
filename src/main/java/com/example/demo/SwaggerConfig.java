29301e86f810f7b658f4e87c470821a6d0c8ae07
khushi-dev <k95578516@gmail.com>
2026-06-18 13:03:02 +0530

    fix swagger bean conflict

--- src/main/java/com/example/demo/SwaggerConfig.java
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,23 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("E-Commerce API")
                         .version("1.0")
-                        .description("API Documentation"));
+                        .description("Production API"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
